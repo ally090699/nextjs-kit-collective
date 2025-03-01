cdf7db5a60f3557c6030fcf7ede580d5832eb0b1
Allison So <aso43@uwo.ca>
2025-02-28 20:36:17 -0500

    changed api base url for deployment

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:3000";
+export const API_BASE_URL = "https://nextjs-kit-collective.onrender.com";
 
 // http://localhost:3000
 // https://nextjs-kit-collective.onrender.com
